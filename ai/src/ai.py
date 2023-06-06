f6234d990a4f18bd71d5c5807d23067e70b61677
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-06-06 13:27:04 +0200

    connect with arguments

--- ai/src/ai.py
+++ ai/src/ai.py
@@ -9,23 +9,35 @@ import socket
 import sys
 from parse_arg import setup_data
 
-data = setup_data()
 
-print(data)
-sys.exit(0)
+# One unit of food allows them to live for 126 units of time
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(('localhost', 4242))
+# It is not necessary for the players to be on the same team; they only need to be of the same level
 
-while (1):
-    response = server.recv(1024).decode()
-    print("'" + response + "'")
-    if (response == "WELCOME\n"):
-        print("Connected to the server")
-        server.send("START\n".encode())
-    message = input("Enter a message to send to the server: ")
-    if (message != ""):
-        server.send(message.encode())
-    if (message == "exit"):
-        break
 
+def main():
+    data = setup_data()
+    print(data)
+    # data["team_name"]
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.connect((data["host"], data["port"]))
+    except ConnectionRefusedError:
+        print("Error: Connection refused to: " + data["host"] + ":" + str(data["port"]))
+        sys.exit(84)
+
+    while (1):
+        response = server.recv(1024).decode()
+        print("'" + response + "'")
+        if (response == "WELCOME\n"):
+            print("Connected to the server")
+            server.send("START\n".encode())
+        message = input("Enter a message to send to the server: ")
+        if (message != ""):
+            server.send(message.encode())
+        if (message == "exit"):
+            break
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
