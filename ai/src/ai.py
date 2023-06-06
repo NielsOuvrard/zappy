ceb48fe72d42232def7adabf76ea976d68ed01b5
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-06-06 14:33:12 +0200

    create user, and look

--- ai/src/ai.py
+++ ai/src/ai.py
@@ -7,6 +7,7 @@
 
 import socket
 import sys
+from time import sleep
 from parse_arg import setup_data
 
 
@@ -14,10 +15,20 @@ from parse_arg import setup_data
 
 # It is not necessary for the players to be on the same team; they only need to be of the same level
 
+# the client sends the "look" command
+# receives :
+# [ player , object-on-tile1 , ... , object-on-tileP ,...]
+
+
+# sond transmission
+# perceive : the direction the sound + subsequent message.
+
+# In the beginning of the game, a team has n slot available
+# # (represented on the board by an egg waiting for a client to connect).
+# so...
 
 def main():
     data = setup_data()
-    print(data)
     # data["team_name"]
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -26,12 +37,26 @@ def main():
         print("Error: Connection refused to: " + data["host"] + ":" + str(data["port"]))
         sys.exit(84)
 
+    response = server.recv(1024).decode() # WELCOME\n
+    server.send((data["team_name"] + "\n").encode())
+    response = server.recv(1024).decode()
+    print("recieve '" + response + "'")
+
+    # recive :
+    # nmb_authorized_remaining_clients_in_team size_x size_y
+    nmb_authorized_remaining_clients_in_team = int(response.split("\n")[0])
+    size_x = int(response.split("\n")[1].split(" ")[0])
+    size_y = int(response.split("\n")[1].split(" ")[1])
+    # [ player food deraumere, food food food linemate phiras, food food food linemate mendiane, food deraumere ]
+
+    # server.send("Fordward\n".encode())
+    server.send("Look\n".encode())
+    response = server.recv(1024).decode()
+    print("'" + response + "'")
+
     while (1):
         response = server.recv(1024).decode()
         print("'" + response + "'")
-        if (response == "WELCOME\n"):
-            print("Connected to the server")
-            server.send("START\n".encode())
         message = input("Enter a message to send to the server: ")
         if (message != ""):
             server.send(message.encode())
