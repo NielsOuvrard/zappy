981a44af952f87b9d9cc53dce556291cb32b9529
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-06-06 17:02:11 +0200

    begin algo, but no GUI to test it -_-

--- ai/src/ai.py
+++ ai/src/ai.py
@@ -2,13 +2,14 @@
 ## EPITECH PROJECT, 2023
 ## B-YEP-400-MAR-4-1-zappy-kenan.blasius
 ## File description:
-## ai
+## main
 ##
 
 import socket
 import sys
 from time import sleep
 from parse_arg import setup_data
+from Player import Player
 
 
 # One unit of food allows them to live for 126 units of time
@@ -27,9 +28,7 @@ from parse_arg import setup_data
 # # (represented on the board by an egg waiting for a client to connect).
 # so...
 
-def main():
-    data = setup_data()
-    # data["team_name"]
+def connect_to_server(data):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server.connect((data["host"], data["port"]))
@@ -42,26 +41,63 @@ def main():
     response = server.recv(1024).decode()
     print("recieve '" + response + "'")
 
-    # recive :
-    # nmb_authorized_remaining_clients_in_team size_x size_y
-    nmb_authorized_remaining_clients_in_team = int(response.split("\n")[0])
+    id = int(response.split("\n")[0])
     size_x = int(response.split("\n")[1].split(" ")[0])
     size_y = int(response.split("\n")[1].split(" ")[1])
+    player = Player(size_x, size_y, id, server)
+    return player
+
+# 1
+# 4
+# 9
+# 16
+# 25
+
+# issue [AI] Handle map
+def fill_map(player: Player, look: list, size: int, incr: int = 1):
+    for i in range(size):
+        player.map.append(look[i]) # to change
+    if look.length() > size:
+        fill_map(player, look[size:], size + incr, incr + 2)
+
+
+def simple_algo_eat(player):
+    look = player.look()
+    print(look)
+    if (look[0] == "food"):
+        res = player.forward()
+        print("forward", res)
+        res = player.take("food")
+        print("food", res)
+        return
+    else:
+        res = player.left()
+        print("left", res)
+        simple_algo_eat(player)
+
+['player', 'mendiane', 'mendiane', 'linemate', 'food', 'food']
+def main():
+    data = setup_data()
+    player = connect_to_server(data)
+
+    while (1):
+        simple_algo_eat(player)
+
     # [ player food deraumere, food food food linemate phiras, food food food linemate mendiane, food deraumere ]
 
     # server.send("Fordward\n".encode())
-    server.send("Look\n".encode())
-    response = server.recv(1024).decode()
-    print("'" + response + "'")
+    # server.send("Look\n".encode())
+    # response = server.recv(1024).decode()
+    # print("'" + response + "'")
 
-    while (1):
-        response = server.recv(1024).decode()
-        print("'" + response + "'")
-        message = input("Enter a message to send to the server: ")
-        if (message != ""):
-            server.send(message.encode())
-        if (message == "exit"):
-            break
+    # while (1):
+    #     response = server.recv(1024).decode()
+    #     print("'" + response + "'")
+    #     message = input("Enter a message to send to the server: ")
+    #     if (message != ""):
+    #         server.send(message.encode())
+    #     if (message == "exit"):
+    #         break
     sys.exit(0)
 
 if __name__ == "__main__":
