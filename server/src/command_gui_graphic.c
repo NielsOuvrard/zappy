ca2ea02661afdd69e362436a11aea8d86638d6e2
Kenan Blasius <kenan.blasius@epitech.eu>
2023-06-22 12:33:11 +0200

    server: fix bct x and y invert in connection and mct command

--- server/src/command_gui_graphic.c
+++ server/src/command_gui_graphic.c
@@ -19,7 +19,7 @@ struct my_string_s *buffer)
         for (int j = 0; j < vector_length(vector_get(g->map, i)); j++) {
             struct tile_s *tile = vector_get(vector_get(g->map, i), j);
             send_to_client(client, "bct %d %d %d %d %d %d %d %d %d\n",
-            i, j, (g->ai_spawn ? tile->food : 0), tile->linemate,
+            j, i, (g->ai_spawn ? tile->food : 0), tile->linemate,
             tile->deraumere, tile->sibur, tile->mendiane, tile->phiras,
             tile->thystame);
         }
