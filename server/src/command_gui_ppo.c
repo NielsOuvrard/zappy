b4dde05ebd3260a227c393c4aeb68328125090fb
Kenan Blasius <kenan.blasius@epitech.eu>
2023-06-23 20:02:03 +0200

    server: fix plv/ppo command

--- server/src/command_gui_ppo.c
+++ server/src/command_gui_ppo.c
@@ -15,7 +15,13 @@ struct my_string_s *buffer)
     if (vector_length(args) != 2)
         return;
     int id = atoi(((struct my_string_s *)vector_get(args, 1))->str);
-    struct client_s *target = vector_get(g->clients, id);
+    struct client_s *target = NULL;
+    for (int i = 0; i < vector_length(g->clients); i++) {
+        target = vector_get(g->clients, i);
+        if (target->client_nb == id)
+            break;
+        target = NULL;
+    }
     if (target == NULL || target->is_gui || target->team == NULL)
         return;
     send_to_client(client, "ppo %d %d %d %d\n", id, target->posx,
